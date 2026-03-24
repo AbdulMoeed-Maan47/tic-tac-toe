15e9dc678b0dfc60ae72f9fe8c7b2dc1416dfa4d
AbdulMoeed-Maan47 <abdulmoeedmn@gmail.com>
2026-03-24 20:43:17 +0500

    Improve game ending UI

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const boxes = document.querySelectorAll(".box");
-const resetContainer = document.querySelector(".reset-container");
-const winningContainer = document.querySelector(".winning-container");
+const gameScreen = document.querySelector(".game-screen");
+const winningScreen = document.querySelector(".winning-screen");
 const winMsg = document.querySelector(".win-msg");
 const resetBtns = document.querySelectorAll(".reset-btn");
 let turnO = true;
@@ -20,11 +20,9 @@ for (let box of boxes) {
   box.addEventListener("click", () => {
     if (turnO) {
       box.innerText = "O";
-      console.log("PlayerO played his move.");
       turnO = false;
     } else {
       box.innerText = "X";
-      console.log("PlayerX played his move.");
       turnO = true;
     }
     box.disabled = true;
@@ -37,8 +35,8 @@ for (let resetBtn of resetBtns) {
   resetBtn.addEventListener("click", () => {
     turnO = true;
     clearBtns();
-    resetContainer.classList.remove("hide");
-    winningContainer.classList.add("hide");
+    gameScreen.classList.remove("hide");
+    winningScreen.classList.add("hide");
     console.clear();
   });
 }
@@ -50,22 +48,21 @@ const disableBtns = () => {
 };
 
 const showWinner = (winner) => {
-  resetContainer.classList.add("hide");
-  winningContainer.classList.remove("hide");
+  gameScreen.classList.add("hide");
+  winningScreen.classList.remove("hide");
   winMsg.innerText = `Congratulations! Winner is Player${winner}`;
 };
 
 const checkWinner = () => {
   for (let winPattern of winPatterns) {
-    let pos0Val = boxes[winPattern[0]].innerText;
-    let pos1Val = boxes[winPattern[1]].innerText;
-    let pos2Val = boxes[winPattern[2]].innerText;
+    let pos1Val = boxes[winPattern[0]].innerText;
+    let pos2Val = boxes[winPattern[1]].innerText;
+    let pos3Val = boxes[winPattern[2]].innerText;
 
-    if (pos0Val !== "" && pos1Val !== "" && pos2Val !== "") {
-      if (pos0Val === pos1Val && pos1Val === pos2Val) {
+    if (pos1Val !== "" && pos2Val !== "" && pos3Val !== "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
         disableBtns();
-        showWinner(pos0Val);
-        console.log(`Winner: Player${pos0Val}`);
+        showWinner(pos1Val);
       }
     }
   }
